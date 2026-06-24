3c016d1d8a622aead66c9a46bb77d7449142c095
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-06-23 18:44:52 -0600

    Script Update

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ function logConsole(msg) {
   consoleBox.innerHTML += <div> ${msg}</div>;
   consoleBox.scrollTop = consoleBox.scrollHeight;
 }
+
+consoleInput.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    const command = consoleInput.value.trim();
+    runCommand(command);
+    consoleInput.value = "";
+  }
+});
