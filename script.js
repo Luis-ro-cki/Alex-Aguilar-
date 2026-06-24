d0a3cec98f87b1516b57a1e14ac1b017674422fd
🌸𝐌𝐢𝐤𝐮 ̶𝐂𝐡𝐚𝐧 ̶🩵✨ <apellidos05.nombre05@gmail.com>
2026-06-24 11:18:10 -0600

    Solution for Some Errors

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const consoleBox = document.getElementById("console");
 const consoleInput = document.getElementById("consoleInput");
 
 function logConsole(msg) {
-  consoleBox.innerHTML += <div> ${msg}</div>;
+  consoleBox.innerHTML += `<div>${msg}</div>`;
   consoleBox.scrollTop = consoleBox.scrollHeight;
 }
 
